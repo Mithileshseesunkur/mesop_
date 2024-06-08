3b6b9c6abae8545dcbf7e6e1c7acf8e39255e10c
Mithileshseesunkur <zavallingum@gmail.com>
2024-06-08 18:25:58 +0200

    yes

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import mesop as me
+import mesop.labs as mel
 
 
-@me.page(path="/main")
-def main():
-  me.text("Hello World")
+@me.page(
+  security_policy=me.SecurityPolicy(
+    allowed_iframe_parents=["https://google.github.io"]
+  ),
+  path="/text_to_text",
+  title="Text to Text Example",
+)
+def app():
+  mel.text_to_text(
+    upper_case_stream,
+    title="Text to Text Example",
+    transform_mode="replace"
+  )
+
+
+def upper_case_stream(s: str):
+  return "Echo: " + s
